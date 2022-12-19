25f16d06e0a581b8e04aebdc22f3e89c3ef64c9c
ASanderOB <113809492+ASanderOB@users.noreply.github.com>
2022-12-18 23:35:01 -0300

    Initial commit

---
+++ src/main/java/com/example/novo/NovoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.novo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/novo/NovoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.novo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
